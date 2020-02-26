18deb701f33bbb65c9a68cded470b3813c220559
Hussain Ahmed <haz6798@icloud.com>
2020-02-26 04:23:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 class Dog
 
-    def bark 
-        puts "Woof!"
-    end
+  def bark
 
-    def sit
-        puts "The Dog is sitting"
-    end
+    puts "Woof!"
+end
+
+def sit
+
+  puts "The Dog is sitting"
 
 end
+end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,14 @@
 class Person
 
-    def talk 
-        puts "Hello World!"
-    end
+  def talk
 
-    def walk
-        puts "The Person is walking"
-    end
+    puts "Hello World!"
 
+  end
+
+  def walk
+
+    puts "The Person is walking"
 end
+end
+
